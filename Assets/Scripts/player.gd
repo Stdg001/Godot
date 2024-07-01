560f02d653a4cfc838132ef6cd2e4fd9ebd4516b
Stdg001 <holasoybarbiequetal1090@gmail.com>
2024-07-01 01:39:28 -0500

    Player movement

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -10,33 +10,41 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	var direction = Input.get_axis("left", "right")
+	var direction_x = Input.get_axis("left", "right")
+	var direction_y = Input.get_axis("up", "down")
 	
-	if (is_on_wall_only() and direction) and velocity.y > 0:
-		_AP.play('wall_slide')
-		velocity.y = (gravity * delta) / 0.2
-	elif not is_on_floor():
+	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	if direction:
-		velocity.x = direction * SPEED
-		_SP.flip_h = false if (direction == 1) else true
+		
+	if direction_x and not is_on_wall():
+		
+		Timer.
+		
+		
+		velocity.x = direction_x * SPEED
+		_SP.flip_h = false if (direction_x == 1) else true
 		_AP.play('walk')
+		print('peocess 19')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		_AP.play('idle')
-
+	
+	if (direction_y and direction_x) and is_on_wall():
+		_AP.play('wall_walking')
+		velocity.y = SPEED * direction_y
+	elif direction_x and is_on_wall():
+		_AP.play('wall_idle')
+		velocity.y = 0
+		
+		if direction_y:
+			_AP.play("wall_slide")
+	
 	if Input.is_action_just_pressed('dash'):
-		velocity.x = (SPEED * 4 * direction) if direction else SPEED * 4
+		velocity.x = (SPEED * 4 * direction_x) if direction_x else SPEED * 4
 		_AP.play('dash')
-			
+
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			_AP.play('jump')
-		
-		if direction and is_on_wall_only():
-			velocity.y = SPEED * direction
-			_AP.play('jump')
-			
 	move_and_slide()
